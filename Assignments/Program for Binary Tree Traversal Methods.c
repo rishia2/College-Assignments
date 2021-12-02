8bbc92e4f8fdadfcf977d5ea2c0900ed3c242e53
rishia2 <88873071+rishia2@users.noreply.github.com>
2021-12-03 03:06:29 +0530

    Add files via upload

---
+++ Assignments/Program for Binary Tree Traversal Methods.c
@@ -0,0 +1,86 @@
+// C program for different tree traversals
+#include <stdio.h>
+#include <stdlib.h>
+
+/* A binary tree node has data, pointer to left child
+and a pointer to right child */
+struct node {
+	int data;
+	struct node* left;
+	struct node* right;
+};
+
+/* Helper function that allocates a new node with the
+given data and NULL left and right pointers. */
+struct node* newNode(int data)
+{
+	struct node* node
+		= (struct node*)malloc(sizeof(struct node));
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
+
+	return (node);
+}
+
+/* Given a binary tree, print its nodes according to the
+"bottom-up" postorder traversal. */
+void printPostorder(struct node* node)
+{
+	if (node == NULL)
+		return;
+
+	printPostorder(node->left);
+
+	printPostorder(node->right);
+
+	printf("%d ", node->data);
+}
+
+/* Given a binary tree, print its nodes in inorder*/
+void printInorder(struct node* node)
+{
+	if (node == NULL)
+		return;
+
+	printInorder(node->left);
+
+	printf("%d ", node->data);
+
+	printInorder(node->right);
+}
+
+/* Given a binary tree, print its nodes in preorder*/
+void printPreorder(struct node* node)
+{
+	if (node == NULL)
+		return;
+
+	printf("%d ", node->data);
+
+	printPreorder(node->left);
+
+	printPreorder(node->right);
+}
+
+/* Driver program to test above functions*/
+int main()
+{
+	struct node* root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+
+	printf("\nPreorder traversal of binary tree is \n");
+	printPreorder(root);
+
+	printf("\nInorder traversal of binary tree is \n");
+	printInorder(root);
+
+	printf("\nPostorder traversal of binary tree is \n");
+	printPostorder(root);
+
+	getchar();
+	return 0;
+}
